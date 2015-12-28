5ed7a21e1fb76acc750f48244e91faa89f0f60c5
Roberto Soares <roberto.tech@gmail.com>
2015-12-28 09:21:00 -0200

    replaces chomp function by Data.Text.strip

--- src/GitInternal.hs
+++ src/GitInternal.hs
@@ -1,14 +1,13 @@
 module GitInternal where
 
 import System.Process
-
-chomp :: String -> String
-chomp = head . lines
+import qualified Data.Text as Text
 
 executeCommand :: [String] -> IO String
 executeCommand command = chomp <$> (readProcess process args [])
   where process = head command
         args = tail command
+        chomp = Text.unpack . Text.strip . Text.pack
 
 extractResult :: [(a, [(Int, String)])] -> String
 extractResult = snd . head . snd . head
